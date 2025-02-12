7628581a04b162b7ec9b25136264099fc07cfbc7
Ralfs <ralfs.stabins2006@gmail.com>
2025-02-13 00:08:43 +0200

    kinda decent

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+const BUTTONS = document.querySelectorAll('.button');
+const DISPLAY = document.getElementById('equation');
+
+let equation = "";
+
+BUTTONS.forEach(function (button) {
+    button.addEventListener('click', function () {
+        const value = button.innerHTML;
+
+        if (value === 'C') {
+            equation = "";
+            DISPLAY.innerHTML = "0";
+        } else if (value === '=') {
+            try {
+                equation = eval(equation).toString();
+            } catch (error) {
+                equation = "Error";
+            }
+        } else {
+            equation += value;
+        }
+
+        // If the equation is empty, display "0"
+        if (equation === "") {
+            DISPLAY.innerHTML = "0";
+        } else {
+            DISPLAY.innerHTML = equation;
+        }
+
+        console.log(equation);
+    });
+});
